1efc68417c4e679d457268a32c43d784253e5aaf
Чупинин Антон <chupinin@intervolga.ru>
2023-10-17 18:52:45 +0300

    Добавлено боковое меню

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ btnToggleTheme.addEventListener('click', () => {
   body.classList.toggle('dark-theme');
   body.classList.toggle('light-theme');
 });
+
+
+const btnAsideMenu = document.querySelector('.menu-list-btn');
+const asideMenu = document.querySelector('.aside-menu');
+btnAsideMenu.addEventListener('click', () => {
+  asideMenu.classList.add('show');
+});
+
+const btnCloseAsideMenu = document.querySelector('.aside-menu .close');
+btnCloseAsideMenu.addEventListener('click', () => {
+  asideMenu.classList.remove('show');
+});
