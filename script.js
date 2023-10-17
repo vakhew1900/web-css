f8de12bba60a6a71f3a7e785ac2d59859f8b2c5e
Чупинин Антон <chupinin@intervolga.ru>
2023-10-17 21:42:17 +0300

    добавлен рабочий слайдер

--- script.js
+++ script.js
@@ -16,3 +16,44 @@ const btnCloseAsideMenu = document.querySelector('.aside-menu .close');
 btnCloseAsideMenu.addEventListener('click', () => {
   asideMenu.classList.remove('show');
 });
+
+
+const images = document.querySelectorAll('.slide');
+const sliderLine = document.querySelector('.slider');
+let count = 0;
+let width;
+
+function init() {
+    console.log('resize');
+    width = document.querySelector('.slider-wrapper').offsetWidth;
+    sliderLine.style.width = width * images.length + 'px';
+    images.forEach(item => {
+        item.style.width = width + 'px';
+        item.style.height = 'auto';
+    });
+    rollSlider();
+}
+
+init();
+window.addEventListener('resize', init);
+
+document.querySelector('.btn-next').addEventListener('click', function () {
+    count++;
+    if (count >= images.length) {
+        count = 0;
+    }
+    rollSlider();
+});
+
+document.querySelector('.btn-prev').addEventListener('click', function () {
+    count--;
+    if (count < 0) {
+        count = images.length - 1;
+    }
+    rollSlider();
+});
+
+function rollSlider() {
+    sliderLine.style.transform = 'translate(-' + count * width + 'px)';
+
+}
